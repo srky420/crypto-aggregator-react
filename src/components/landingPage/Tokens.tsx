d079e245d705ea39c2f1a11e6915b0d499bb4677
srky420 <srky420@gmail.com>
2024-04-04 14:43:07 +0500

    Add pagination to tokens component

--- src/components/landingPage/Tokens.tsx
+++ src/components/landingPage/Tokens.tsx
@@ -6,7 +6,10 @@ import { Button } from "../globals/Button";
 export const Tokens = () => {
 
     // State for API Data
-    const [data, setData] = useState(cryptoListings.data.slice(0, 10));
+    const [data, setData] = useState(cryptoListings.data);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 10;
+    const totalPages = Math.ceil(cryptoListings.data.length / pageSize);
 
     // USD Currency format
     const USDollar = new Intl.NumberFormat('en-US', {
@@ -16,13 +19,21 @@ export const Tokens = () => {
 
     // Effect
     useEffect(() => {
-        // Fetch crypto data
-        // const url = 'http://localhost:3000/cryptocurrency';
-        // fetch(url)
-        // .then(res => res.json())
-        // .then(data => setData(data))
-        // .catch(err => console.log(err));
-    })
+
+        const paginatedData = cryptoListings.data.slice((currentPage - 1) * pageSize, ((currentPage - 1) * pageSize) + pageSize);
+        setData(paginatedData);
+
+    }, [currentPage])
+
+    // Next page
+    const nextPage = () => {
+        setCurrentPage(prev => prev < totalPages ? ++prev : totalPages)
+    }
+
+    // Prev page
+    const prevPage = () => {
+        setCurrentPage(prev => prev === 1 ? 1 : --prev);
+    }
 
     return (
         <section className="container mx-auto px-5 md:px-20 lg:px-40 my-20 lg:my-40">
@@ -47,13 +58,14 @@ export const Tokens = () => {
                             {(item.quote.USD.percent_change_24h).toFixed(2)}%
                         </h6>
                         <h6 className="hidden sm:block">{USDollar.format(item.quote.USD.market_cap)}</h6>
-                        <a href="" className="justify-self-center text-indigo-600 underline underline-offset-4">See More</a>
+                        <a href="" className="justify-self-center text-indigo-600 underline underline-offset-4 hover:opacity-80">See More</a>
                     </div>
                 )}
             </div>
             <div className="w-fit mx-auto my-10">
-                <Button variant="light" size="" text={`<i class="fa-solid fa-chevron-left"></i>`} />
-                <Button variant="light" size="" text={`<i class="fa-solid fa-chevron-right"></i>`} />
+                <Button variant="light" size="" text={`<i class="fa-solid fa-chevron-left"></i>`} onClick={prevPage} />
+                <Button variant="light" size="" text={`<i class="fa-solid fa-chevron-right"></i>`} onClick={nextPage} />
+                <p className="text-center">Showing {currentPage} of {totalPages}</p>
             </div>
         </section>
     )
